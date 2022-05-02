affe88fa9274cea830141a34e4266dd5834bbe94
William Foody <foodyw@gmail.com>
2022-05-02 00:33:04 -0400

    Create necessary components and general project structure

---
+++ src/components/MenuItemCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './MenuItemCard.module.css';
+
+const MenuItemCard = () => {
+
+    return (
+        <div>
+            <div>card</div>
+        </div>
+    )
+}
+
+export default MenuItemCard;
