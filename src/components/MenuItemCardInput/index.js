1dc2d36da7838f2ec92d59c61a7a0c1b6f22061d
William Foody <foodyw@gmail.com>
2022-05-04 14:33:27 -0400

    Create 'add item' capability

--- src/components/MenuItemCardInput/index.js
+++ src/components/MenuItemCardInput/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './MenuItemCardInput.module.css';
 
-const MenuItemCardInput = () => {
+const MenuItemCardInput = (props) => {
+
     return (
-        <div>
+        <div className={styles.inputDiv}>
             <form className={styles.inputForm}>
-                <input type='text' placeholder='Title'/>
-                <input type='text' placeholder='Price'/>
-                <input type='text' placeholder='Description'/>
-                <input type='text' placeholder='image URL'/>
+                <input type='text' placeholder='Title' name='title' onChange={props.handleAddItem} value={props.item.title} />
+                <input type='text' placeholder='Price' name='price' onChange={props.handleAddItem} value={props.item.price} />
+                <input type='text' placeholder='Description' name='description' onChange={props.handleAddItem} value={props.item.description} />
+                <input type='text' placeholder='Image URL' name='imageUrl' onChange={props.handleAddItem} value={props.item.imageUrl} />
+                <button type='button' onClick={props.handleSubmit}>+</button>
             </form>
         </div>
     );
--- src/components/MenuItemList/index.js
+++ src/components/MenuItemList/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styles from './MenuItemList.module.css';
+// import MenuItemCardInput from '../MenuItemCardInput';
+import MenuItemCard from '../MenuItemCard';
 
-const MenuItemList = () => {
+const MenuItemList = (props) => {
     return (
-        <div>list of items</div>
+        <div>
+            <ul>
+                {props.menuList.map((item, index) => {
+                    return (
+                        <li><MenuItemCard  title={item.title} price={item.price} description={item.description} imageUrl={item.imageUrl} /></li>
+                    )
+                })}
+            </ul>
+        {/* <MenuItemCard title='hello' price='2'/> */}
+        </div>
     );
 }
 
