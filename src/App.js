af6b5ce1ea5e3f672107585d2863afdd0f7e103c
William Foody <foodyw@gmail.com>
2022-05-04 14:59:26 -0400

    Add 'delete item' capability

--- src/App.js
+++ src/App.js
@@ -1,65 +1,72 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 import MenuItemCardInput from './components/MenuItemCardInput';
 import MenuItemList from './components/MenuItemList';
 
 function App() {
-  
+
   const [item, setItem] = useState({ title: '', price: '', description: '', imageUrl: '' });
   const [menuList, setMenuList] = useState([
-      {
-          title: 'Wings',
-          price: '20',
-          description: 'hot',
-          imageUrl: '#',
-      },
-      {
-          title: 'Salad',
-          price: '10',
-          description: 'good',
-          imageUrl: '#',
-      },
-      {
-          title: 'Pasta',
-          price: '15',
-          description: 'cheesy',
-          imageUrl: '#',
-      },
-      {
-          title: 'Fries',
-          price: '5',
-          description: 'salty',
-          imageUrl: '#',
-      }
+    {
+      title: 'Wings',
+      price: '20',
+      description: 'hot',
+      imageUrl: '#',
+    },
+    {
+      title: 'Salad',
+      price: '10',
+      description: 'good',
+      imageUrl: '#',
+    },
+    {
+      title: 'Pasta',
+      price: '15',
+      description: 'cheesy',
+      imageUrl: '#',
+    },
+    {
+      title: 'Fries',
+      price: '5',
+      description: 'salty',
+      imageUrl: '#',
+    }
   ])
 
   const handleAddItem = (e) => {
-      setMenuList([...menuList]);
-      setItem({ ...item, [e.target.name]: e.target.value });
-      console.log(item)
+    setMenuList([...menuList]);
+    setItem({ ...item, [e.target.name]: e.target.value });
+    console.log(item)
   };
 
   const handleSubmit = () => {
-      if (item.title && item.price && item.description) {
-          setMenuList([...menuList, item]);
-          setItem({ title: '', price: '', description: '', imageUrl: '' })
-      } else {
-          alert('Please complete all fields before submitting')
-      }
+    if (item.title && item.price && item.description) {
+      setMenuList([...menuList, item]);
+      setItem({ title: '', price: '', description: '', imageUrl: '' })
+    } else {
+      alert('Please complete all fields before submitting')
+    }
+  };
+
+  const handleDelete = (index) => {
+    let newMenuList = menuList;
+    newMenuList.splice(index, 1);
+    console.log(newMenuList)
+    setMenuList([...newMenuList]);
   };
 
   return (
     <div className={styles.App}>
       <h1>Sweenz Bistro</h1>
-      <MenuItemCardInput 
+      <MenuItemCardInput
         item={item}
         setItem={setItem}
         menuList={menuList}
         setMenuList={setMenuList}
         handleAddItem={handleAddItem}
         handleSubmit={handleSubmit}
-        />
-      <MenuItemList menuList={menuList} title={item.title} price={item.price} description={item.description} imageUrl={item.imageUrl} />
+      />
+      <MenuItemList menuList={menuList} title={item.title} price={item.price} description={item.description} imageUrl={item.imageUrl} handleDelete={handleDelete} />
     </div>
   );
 }
--- src/components/MenuItemList/index.js
+++ src/components/MenuItemList/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import styles from './MenuItemList.module.css';
-// import MenuItemCardInput from '../MenuItemCardInput';
 import MenuItemCard from '../MenuItemCard';
 
 const MenuItemList = (props) => {
     return (
-        <div>
-            <ul>
+        <div className={styles.menuList}>
                 {props.menuList.map((item, index) => {
                     return (
-                        <li><MenuItemCard  title={item.title} price={item.price} description={item.description} imageUrl={item.imageUrl} /></li>
+                        <MenuItemCard  title={item.title} price={item.price} description={item.description} imageUrl={item.imageUrl} handleDelete={props.handleDelete} index={index} />
                     )
                 })}
-            </ul>
-        {/* <MenuItemCard title='hello' price='2'/> */}
         </div>
     );
 }
