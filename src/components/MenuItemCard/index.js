d9cc9851050941bc09c0aaf40e5ff710266aa6ed
William Foody <foodyw@gmail.com>
2022-05-05 12:42:38 -0400

    Add inline editing to menu items

--- src/components/MenuItemCard/index.js
+++ src/components/MenuItemCard/index.js
@@ -3,17 +3,59 @@ import styles from './MenuItemCard.module.css';
 
 const MenuItemCard = (props) => {
 
+    const price = parseFloat(props.price);
+    const formattedPrice = `$${price.toFixed(2)}`
+
     return (
-        <div className={styles.card}>
-            <div>
-                <img src='https://picsum.photos/150' />
-            </div>
-            <div>
-                <div>{props.title}</div>
-                <div>{props.price}</div>
-                <div>{props.description}</div>
-                <button type='submit' onClick={() => window.confirm(`Are you sure you want to delete ${props.title} from the menu?`) ? props.handleDelete(props.index) : null }>X</button>
-            </div>
+        <div>
+            {props.editId === props.id ?
+                <div className={styles.editCard}>
+                    <input
+                        type='text'
+                        name='title'
+                        onChange={props.handleEditingText}
+                        value={props.editingText.title}
+                        placeholder={'Title'}
+                    />
+                    <input
+                        type='number'
+                        name='price'
+                        onChange={props.handleEditingText}
+                        value={props.editingText.price}
+                        placeholder={'Price'}
+                    />
+                    <input
+                        type='text'
+                        name='description'
+                        onChange={props.handleEditingText}
+                        value={props.editingText.description}
+                        placeholder={'Description'}
+                    />
+                    <input
+                        type='text'
+                        name='imageUrl'
+                        onChange={props.handleEditingText}
+                        placeholder={"Image URL"}
+                        value={props.editingText.imageUrl}
+                    />
+                    <button type='submit' onClick={() => props.handleEditSubmit(props.id)}>Save</button>
+                    <button type='submit' onClick={() => window.confirm(`Are you sure you want to delete ${props.title} from the menu?`) ? props.handleDelete(props.index) : null}>X</button>
+                    <button type='submit' onClick={() => props.setEditId(null)}>Cancel</button>
+                </div>
+                :
+                <div className={styles.card}>
+                    <div>
+                        <img src='https://picsum.photos/150' />
+                    </div>
+                    <div>
+                        <div>{props.title}</div>
+                        <div>{props.price ? formattedPrice : null}</div>
+                        <div>{props.description}</div>
+                        <button onClick={() => props.setEditId(props.id)}>Edit</button>
+                        <button type='submit' onClick={() => window.confirm(`Are you sure you want to delete ${props.title} from the menu?`) ? props.handleDelete(props.index) : null}>X</button>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
--- src/components/MenuItemCardInput/index.js
+++ src/components/MenuItemCardInput/index.js
@@ -6,11 +6,40 @@ const MenuItemCardInput = (props) => {
     return (
         <div className={styles.inputDiv}>
             <form className={styles.inputForm}>
-                <input type='text' placeholder='Title' name='title' onChange={props.handleAddItem} value={props.item.title} />
-                <input type='text' placeholder='Price' name='price' onChange={props.handleAddItem} value={props.item.price} />
-                <input type='text' placeholder='Description' name='description' onChange={props.handleAddItem} value={props.item.description} />
-                <input type='text' placeholder='Image URL' name='imageUrl' onChange={props.handleAddItem} value={props.item.imageUrl} />
-                <button type='button' onClick={props.handleSubmit}>+</button>
+                <input
+                    type='text'
+                    placeholder='Title'
+                    name='title'
+                    onChange={props.handleAddItem}
+                    value={props.item.title}
+                />
+                <input
+                    type='number'
+                    placeholder='Price'
+                    name='price'
+                    onChange={props.handleAddItem}
+                    value={props.item.price}
+                />
+                <input
+                    type='text'
+                    placeholder='Description'
+                    name='description'
+                    onChange={props.handleAddItem}
+                    value={props.item.description}
+                />
+                <input
+                    type='text'
+                    placeholder='Image URL'
+                    name='imageUrl'
+                    onChange={props.handleAddItem}
+                    value={props.item.imageUrl}
+                />
+                <button
+                    type='button'
+                    onClick={props.handleSubmit}
+                >
+                    +
+                </button>
             </form>
         </div>
     );
